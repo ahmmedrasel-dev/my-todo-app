ec3f10bd3f8c3d9381a977ea8d873d3d00267104
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-06-15 00:24:36 +0300

    user lggin and firebase auth install

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './component/Home';
 import Login from './component/Login';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Register from './component/Register';
 
 function App() {
   return (
@@ -9,7 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
+      <ToastContainer />
     </>
   );
 }
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,48 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
 const Login = () => {
-  return (
-    <div>
 
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    hookError,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  // Sign In with Email And Password 
+  const handleSubmit = async e => {
+    e.preventDefault()
+    if (email.length !== 0 && password.length) {
+      await signInWithEmailAndPassword(email, password)
+    } else {
+      toast.error('Email & Password is empaty.')
+    }
+  }
+
+
+  return (
+    <div className='md:p-20'>
+      <div className='md:w-2/6 sm:w-4/6 md:h-82 md:mx-auto flex flex-col justify-center bg-slate-100 border-2 rounded-3xl'>
+        <div className='text-center my-8'>
+          <h2 className='text-4xl text-cyan-500'>My Todo App</h2>
+          <p className=''>Please login with your email and Password.</p>
+        </div>
+
+        <form className='sm:w-4/4 md:w-3/4 mx-auto mt-4' onSubmit={handleSubmit}>
+          <input className='block w-full p-3 rounded-md text-xl' type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+          <input className='block w-full p-3 my-4 rounded-md text-xl' type="current-password" placeholder='Password' onBlur={(e) => setPassword(e.target.value)} />
+          <button className='w-full bg-sky-500 p-3 mb-4 rounded-md text-xl text-white'>Login</button>
+          <p className='text-center mb-2'> <Link className='underline text-blue-400' to='/reset-password'>Forget Password?</Link></p>
+
+          <p className='text-center mb-2'>Don't have Account? <Link className='underline text-blue-400' to='/register'>Create an account.</Link></p>
+
+        </form>
+      </div>
     </div>
   );
 };
